caf0a023875986d576066873efadbcf73a14f6ab
Rajan Goyal <goyal.rajan@yahoo.com>
2022-03-20 16:22:42 -0400

    Added functionality to get data from chrome storage and append to buttons

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 var Elements = ["C", "C++", "Java", "Python",
     "R", "Perl", "C#", "PHP"];
 
-chrome.storage.local.get(['key'], function(result) {
-    // alert(Elements.length);
-    Elements.push(result.key);
-    // alert(typeof result.key);
-    // alert(Elements.length);
-    //alert(result.key);
-    addButtons();
-    //alert('Value currently is ' + result.key);
-});
-function addButtons() {
+getFromStroage();
+
+
+// Gets objects from storage
+function getFromStroage() {
+    var value = "33";
+    chrome.storage.local.get(['key'],
+        function (result) {
+            value = result.key;
+            Elements.push(value);
+            addButton();
+        }
+    );
+}
+
+// Adds button to the page
+function addButton() {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < Elements.length; i++) {
-        //alert(Elements[i]);
+        // alert(Elements[i]);
         var e = document.createElement("button");
         e.innerHTML = Elements[i];
         e.id = Elements[i];
-        e.addEventListener('click', event => {
-            //handle click
-            myFunction(e.id);
-        })
+        /*        e.addEventListener('click', event => {
+                    //handle click
+                    myFunction(e.id);
+                })*/
         fragment.appendChild(e);
     }
     var UlElement = document.getElementById('LanguagesDiv');
     UlElement.appendChild(fragment);
+// Adds copy function to each button
+    document.querySelectorAll('button').forEach(item => {
+        item.addEventListener('click', event => {
+            //handle click
+            myFunction(item.id);
+        })
+    });
+
 }
 
-document.querySelectorAll('button').forEach(item => {
-    item.addEventListener('click', event => {
-        //handle click
-        myFunction(item.id);
-    })
 
-})
 function myFunction(id) {
     var r = document.createRange();
     r.selectNode(document.getElementById(id));
@@ -41,13 +50,4 @@ function myFunction(id) {
     window.getSelection().addRange(r);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
-    var value = "iad7122";
-    var key ="";
-
-    chrome.storage.local.set({key: value},
-        function() {
-            //alert('Value is set to ' + value);
-        });
-
-
 }
